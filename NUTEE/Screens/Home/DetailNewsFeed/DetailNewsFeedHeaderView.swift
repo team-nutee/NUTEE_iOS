9a5e9e5e2d6ed4e1851ab18ddbd1d5b6ce2e72ff
Kim HeeJae <iowa329@gmail.com>
2020-07-27 14:51:46 +0900

    [CREATE] Add NewsFeed & DetailNewsFeed file without storyboard and xib

---
+++ NUTEE/Screens/Home/DetailNewsFeed/DetailNewsFeedHeaderView.swift
@@ -0,0 +1,9 @@
+//
+//  DetailNewsFeedHeaderView.swift
+//  NUTEE
+//
+//  Created by Hee Jae Kim on 2020/07/24.
+//  Copyright © 2020 Nutee. All rights reserved.
+//
+
+import Foundation
---
+++ NUTEE/Screens/Home/DetailNewsFeed/ReplyCell.swift
@@ -0,0 +1,9 @@
+//
+//  DetailNewsFeedTVCell.swift
+//  NUTEE
+//
+//  Created by Hee Jae Kim on 2020/07/24.
+//  Copyright © 2020 Nutee. All rights reserved.
+//
+
+import Foundation
