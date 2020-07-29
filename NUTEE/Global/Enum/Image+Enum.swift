b299322e1f0067fd3d041b87a1af676adb327ca7
junhyeon <oh.junhyeon95@gmail.com>
2020-07-29 15:08:04 +0900

    [Create] Enum 파일 생성

---
+++ NUTEE/Global/Enum/Image+Enum.swift
@@ -0,0 +1,13 @@
+//
+//  Image+Enum.swift
+//  NUTEE
+//
+//  Created by 오준현 on 2020/07/29.
+//  Copyright © 2020 Nutee. All rights reserved.
+//
+
+// 이미지를 지정해주는 enum입니다.
+
+enum Image {
+    
+}
---
+++ NUTEE/Global/Enum/KeyChainKeys+Enum.swift
@@ -0,0 +1,13 @@
+//
+//  KeyChains+Enum.swift
+//  NUTEE
+//
+//  Created by 오준현 on 2020/07/29.
+//  Copyright © 2020 Nutee. All rights reserved.
+//
+
+import Foundation
+
+enum KeyChainKeys {
+    
+}
