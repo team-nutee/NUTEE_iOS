d4af886fbe0dce674fa4a27c6839df232234a622
junhyeon <oh.junhyeon95@gmail.com>
2020-07-29 15:34:43 +0900

    [Create] Extension 생성 및 설정

---
+++ NUTEE/Global/Extensions/CALayer+Extension.swift
@@ -0,0 +1,52 @@
+//
+//  CALayer+Extension.swift
+//  NUTEE
+//
+//  Created by 오준현 on 2020/07/29.
+//  Copyright © 2020 Nutee. All rights reserved.
+//
+
+import UIKit
+
+extension CALayer {
+    
+    func addBorder(_ arr_edge: [UIRectEdge], color: UIColor, width: CGFloat) {
+        for edge in arr_edge {
+            let border = CALayer()
+            switch edge {
+            case UIRectEdge.top:
+                border.frame = CGRect.init(x: 0, y: 0,
+                                           width: frame.width, height: width)
+                break
+            case UIRectEdge.bottom:
+                border.frame = CGRect.init(x: 0, y: frame.height - width,
+                                           width: frame.width, height: width)
+                break
+            case UIRectEdge.left:
+                border.frame = CGRect.init(x: 0, y: 0,
+                                           width: width, height: frame.height)
+                break
+            case UIRectEdge.right:
+                border.frame = CGRect.init(x: frame.width - width, y: 0,
+                                           width: width, height: frame.height)
+                break
+            default:
+                break
+            }
+            border.backgroundColor = color.cgColor;
+            self.addSublayer(border)
+        }
+    }
+    
+    func applyShadow(color: UIColor = .black,
+                     alpha: Float = 0.5,
+                     x: CGFloat = 0,
+                     y: CGFloat = 2,
+                     blur: CGFloat = 4) {
+        shadowColor = color.cgColor
+        shadowOpacity = alpha
+        shadowOffset = CGSize(width: x, height: y)
+        shadowRadius = blur / 2.0
+    }
+    
+}
