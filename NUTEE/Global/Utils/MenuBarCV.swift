ab628f34321d6e34444875da5d2f280b61da08da
Kim HeeJae <iowa329@gmail.com>
2020-08-01 15:30:35 +0900

    [UPDATE] 메뉴바 형태 NewsFeed 화면 구현

--- NUTEE/Global/Utils/MenuBarCV.swift
+++ NUTEE/Global/Utils/MenuBarCV.swift
@@ -10,12 +10,173 @@ import UIKit
 
 class MenuBarCV : UIView {
     
+    // MARK: - UI components
     
+    lazy var menuBarCollectionView = UICollectionView(frame: .zero, collectionViewLayout: UICollectionViewFlowLayout())
+    let positionBarView = UIView()
+    
+    // MARK: - Variables and Properties
+    
+    var homeVC: HomeVC?
+    
+    // MARK: - Life Cycle
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        _ = menuBarCollectionView.then {
+            let layout = UICollectionViewFlowLayout()
+            layout.scrollDirection = .horizontal
+            layout.minimumLineSpacing = 0
+            layout.minimumInteritemSpacing = 0
+            
+            $0.collectionViewLayout = layout
+            
+            $0.delegate = self
+            $0.dataSource = self
+            
+            $0.register(MenuBarCVCell.self, forCellWithReuseIdentifier: "MenuBarCVCell")
+            
+            addSubview($0)
+            $0.snp.makeConstraints {
+                $0.top.equalToSuperview()
+                $0.left.equalToSuperview()
+                $0.right.equalToSuperview()
+                $0.bottom.equalToSuperview()
+            }
+        }
+    }
+    
+    // 레이아웃이 형성된 뒤에 불려지는 함수
+    override func layoutSubviews() {
+         super.layoutSubviews()
+        
+        _ = menuBarCollectionView.then {
+            $0.backgroundColor = .white
+            
+            // 최초 실행 시 선택되어 있는 위치 지정
+            let indexPath = IndexPath(item: 0, section: 0)
+            $0.selectItem(at: indexPath, animated: false, scrollPosition: .left)
+        }
+        
+        _ = positionBarView.then {
+            $0.backgroundColor = .nuteeGreen
+            
+            addSubview($0)
+            $0.snp.makeConstraints {
+                $0.height.equalTo(4)
+                let width = menuBarCollectionView.frame.size.width / CGFloat(menuList.count) - 20
+                $0.width.equalTo(width)
+                $0.left.equalToSuperview().offset(10)
+                $0.bottom.equalToSuperview()
+            }
+        }
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    // MARK: - Variables and Properties
+    
+    let menuList = ["추천 게시글", "내 전공", "전체 게시글"]
+    
+    // MARK: - Helper
+}
+    
+
+// MARK: - Menu Bar CollectionView
+
+extension MenuBarCV : UICollectionViewDelegate { }
+extension MenuBarCV : UICollectionViewDelegateFlowLayout {
+    
+    func collectionView(_ collectionView:UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let menuCount = CGFloat(menuList.count)
+        
+        return CGSize(width: self.frame.width/menuCount, height: collectionView.frame.height)
+    }
     
 }
 
-class MenuBarCell : UICollectionViewCell {
+extension MenuBarCV : UICollectionViewDataSource {
     
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return menuList.count
+    }
     
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = menuBarCollectionView.dequeueReusableCell(withReuseIdentifier: "MenuBarCVCell", for: indexPath) as! MenuBarCVCell
+        
+        cell.initCell()
+        cell.addContentView()
+        
+        cell.menuTitle.text = menuList[indexPath.item]
+        
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        self.homeVC?.scrollToMenuIndex(menuIndex: indexPath.item)
+    }
+}
+
+
+// MARK: - Menu Bar CollectionView Cell Definition
+
+class MenuBarCVCell : UICollectionViewCell {
+    
+    // MARK: - UI components
+    
+    var menuTitle = UILabel()
+    
+    // MARK: - Helper
+    
+    func initCell() {
+        _ = menuTitle.then {
+            $0.font = .systemFont(ofSize: 15)
+            $0.textColor = .gray
+        }
+    }
+    
+    func addContentView() {
+        contentView.addSubview(menuTitle)
+        
+        menuTitle.snp.makeConstraints {
+            $0.centerX.equalTo(contentView)
+            $0.centerY.equalTo(contentView)
+        }
+    }
+    
+    override var isHighlighted: Bool {
+        didSet {
+            if isSelected == true {
+                _ = menuTitle.then {
+                    $0.font = .boldSystemFont(ofSize: 18)
+                    $0.textColor = .nuteeGreen
+                }
+            } else {
+                _ = menuTitle.then {
+                    $0.font = .systemFont(ofSize: 15)
+                    $0.textColor = .gray
+                }
+            }
+        }
+    }
+    
+    override var isSelected: Bool {
+        didSet {
+            if isSelected == true {
+                _ = menuTitle.then {
+                    $0.font = .boldSystemFont(ofSize: 18)
+                    $0.textColor = .nuteeGreen
+                }
+            } else {
+                _ = menuTitle.then {
+                    $0.font = .systemFont(ofSize: 15)
+                    $0.textColor = .gray
+                }
+            }
+        }
+    }
     
 }
--- NUTEE/Screens/Home/HomeVC.swift
+++ NUTEE/Screens/Home/HomeVC.swift
@@ -6,4 +6,156 @@
 //  Copyright © 2020 Nutee. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+import SwiftKeychainWrapper
+
+import SnapKit
+import Then
+
+class HomeVC: UIViewController {
+    
+    // MARK: - UI components
+    
+    let navigationBar = UINavigationBar()
+    let menuBar = MenuBarCV()
+    
+    let newsFeedContainerCollectionView = UICollectionView(frame: .zero, collectionViewLayout: UICollectionViewFlowLayout())
+    
+    // MARK: - Variables and Properties
+    
+    // MARK: - Dummy data
+    
+    // MARK: - Life Cycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.navigationItem.title = "NUTEE"
+        view.backgroundColor = .white
+        
+        setNavigationBar()
+        setMenuBar()
+        setNewsFeedContainerCollectionView()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewDidLoad()
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(false)
+        
+    }
+    
+    // MARK: - Helper
+    
+    func setNavigationBar() {
+        _ = navigationBar.then {
+            let navigationBarAppearence = UINavigationBarAppearance()
+            navigationBarAppearence.shadowColor = .clear
+
+            $0.scrollEdgeAppearance = navigationBarAppearence
+            
+            $0.backgroundColor = .white
+        }
+        
+        view.addSubview(navigationBar)
+        navigationBar.snp.makeConstraints {
+            $0.top.equalToSuperview()
+            $0.left.equalToSuperview()
+            $0.right.equalToSuperview()
+        }
+    }
+    
+    func setMenuBar() {
+        _ = menuBar.then {
+            view.addSubview($0)
+            
+            $0.snp.makeConstraints {
+                let window = UIApplication.shared.windows.first {$0.isKeyWindow}
+                let statusBarHeight = CGFloat(window?.windowScene?.statusBarManager?.statusBarFrame.height ?? 0)
+                let navigationHeight = CGFloat(self.navigationController?.navigationBar.frame.height ?? 0.0)
+                
+                $0.top.equalTo(statusBarHeight + navigationHeight)
+                $0.left.equalTo(view.snp.left)
+                $0.right.equalTo(view.snp.right)
+                $0.height.equalTo(50)
+            }
+            
+            $0.homeVC = self
+        }
+    }
+    
+    func setNewsFeedContainerCollectionView() {
+        _ = newsFeedContainerCollectionView.then {
+            let layout = UICollectionViewFlowLayout()
+            layout.scrollDirection = .horizontal
+            layout.minimumLineSpacing = 0
+            layout.minimumInteritemSpacing = 0
+            
+            $0.collectionViewLayout = layout
+            
+            $0.delegate = self
+            $0.dataSource = self
+            
+            $0.register(FeedContainerCVCell.self, forCellWithReuseIdentifier: "FeedContainerCVCell")
+            
+            view.addSubview($0)
+            $0.snp.makeConstraints {
+                $0.top.equalTo(menuBar.snp.bottom)
+                $0.left.equalTo(view.snp.left)
+                $0.right.equalTo(view.snp.right)
+                $0.bottom.equalTo(view.snp.bottom)
+            }
+            
+            $0.backgroundColor = .white
+            
+            $0.isPagingEnabled = true
+            $0.showsHorizontalScrollIndicator = false
+        }
+    }
+    
+    func scrollToMenuIndex(menuIndex: Int) {
+        let indexPath = IndexPath(item: menuIndex, section: 0)
+        newsFeedContainerCollectionView.scrollToItem(at: indexPath, at: .centeredHorizontally, animated: true)
+    }
+    
+}
+
+// MARK: - NewsFeed CollectionView Container
+
+extension HomeVC : UICollectionViewDelegate { }
+extension HomeVC : UICollectionViewDelegateFlowLayout {
+    
+    func collectionView(_ collectionView:UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: self.view.frame.width, height: collectionView.frame.height)
+    }
+
+}
+
+extension HomeVC : UICollectionViewDataSource {
+    
+    func scrollViewDidScroll(_ scrollView: UIScrollView) {
+        menuBar.positionBarView.frame.origin.x = scrollView.contentOffset.x / CGFloat(menuBar.menuList.count) + 10
+    }
+    
+    func scrollViewWillEndDragging(_ scrollView: UIScrollView, withVelocity velocity: CGPoint, targetContentOffset: UnsafeMutablePointer<CGPoint>) {
+        let menuIndex = Int(targetContentOffset.pointee.x / view.frame.width)
+        let indexPath = IndexPath(item: menuIndex, section: 0)
+        menuBar.menuBarCollectionView.selectItem(at: indexPath, animated: true, scrollPosition: .centeredHorizontally)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return menuBar.menuList.count
+    }
+
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = newsFeedContainerCollectionView.dequeueReusableCell(withReuseIdentifier: "FeedContainerCVCell", for: indexPath) as! FeedContainerCVCell
+
+        cell.homeVC = self
+        cell.setTableView()
+
+        return cell
+    }
+}
