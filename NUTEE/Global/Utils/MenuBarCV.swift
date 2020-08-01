be5259fb03c0b616513a8bcb58741ff84f5a9ef9
Kim HeeJae <iowa329@gmail.com>
2020-08-01 15:28:55 +0900

    [UPDATE] Home 탭 화면 구성을 위한 파일 위치 조정

---
+++ NUTEE/Global/Utils/MenuBarCV.swift
@@ -0,0 +1,21 @@
+//
+//  MenuBar.swift
+//  NUTEE
+//
+//  Created by Hee Jae Kim on 2020/07/31.
+//  Copyright © 2020 Nutee. All rights reserved.
+//
+
+import UIKit
+
+class MenuBarCV : UIView {
+    
+    
+    
+}
+
+class MenuBarCell : UICollectionViewCell {
+    
+    
+    
+}
---
+++ NUTEE/Screens/Home/FeedContainerCVCell.swift
@@ -0,0 +1,9 @@
+//
+//  FeedContainerCVCell.swift
+//  NUTEE
+//
+//  Created by Hee Jae Kim on 2020/07/31.
+//  Copyright © 2020 Nutee. All rights reserved.
+//
+
+import Foundation
