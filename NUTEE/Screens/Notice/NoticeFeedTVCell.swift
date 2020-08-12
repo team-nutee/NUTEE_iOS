6f45e390b399973bc0829525c19be77840795d1b
Kim HeeJae <iowa329@gmail.com>
2020-08-12 18:16:29 +0900

    [UPDATE] NoticeVC 구현을 위한 파일 정리

---
+++ NUTEE/Screens/Notice/NoticeFeedTVCell.swift
@@ -0,0 +1,118 @@
+//
+//  NoticeTVCell.swift
+//  NUTEE
+//
+//  Created by Hee Jae Kim on 2020/08/12.
+//  Copyright © 2020 Nutee. All rights reserved.
+//
+
+import UIKit
+
+import SwiftKeychainWrapper
+
+class NoticeFeedTVCell: UITableViewCell {
+    
+    static let identifier = Identify.NoticeFeedTVCell
+    
+    // MARK: - UI components
+    
+    let fixedNoticeImageView = UIImageView()
+    
+    let titleLabel = UILabel()
+    
+    let dateLabel = UILabel()
+    let authorLabel = UILabel()
+    
+    // MARK: - Variables and Properties
+    
+    // MARK: - Life Cycle
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        
+        initCell()
+        addContentView()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    // MARK: - Helper
+    
+    func initCell () {
+        _ = fixedNoticeImageView.then {
+            $0.image = UIImage(systemName: "pin")
+            $0.tintColor = .red
+            
+            $0.isHidden = true
+        }
+        
+        _ = titleLabel.then {
+            $0.text = "제목제목제목"
+            $0.font = .boldSystemFont(ofSize: 17)
+        }
+        
+        _ = dateLabel.then {
+            $0.text = "2020-08-12"
+            $0.font = .systemFont(ofSize: 11)
+            $0.textColor = .gray
+        }
+        _ = authorLabel.then {
+            $0.text = "교무처"
+            $0.font = .systemFont(ofSize: 11)
+            $0.textColor = .gray
+            $0.sizeToFit()
+        }
+    }
+    
+    func addContentView() {
+        
+        contentView.addSubview(fixedNoticeImageView)
+        
+        contentView.addSubview(titleLabel)
+        
+        contentView.addSubview(dateLabel)
+        contentView.addSubview(authorLabel)
+        
+        
+        fixedNoticeImageView.snp.makeConstraints {
+            $0.width.equalTo(15)
+            $0.height.equalTo(fixedNoticeImageView.snp.width)
+
+            $0.centerY.equalTo(titleLabel)
+            $0.left.equalTo(contentView.snp.left).offset(10)
+        }
+        
+        titleLabel.snp.makeConstraints {
+            $0.top.equalTo(contentView.snp.top).offset(20)
+            $0.left.equalTo(fixedNoticeImageView.snp.right).offset(10)
+            $0.right.equalTo(contentView.snp.right).inset(20)
+        }
+        
+        dateLabel.snp.makeConstraints {
+            $0.width.equalTo(65)
+            
+            $0.top.equalTo(titleLabel.snp.bottom).offset(10)
+            $0.left.equalTo(titleLabel.snp.left)
+            $0.bottom.equalTo(contentView.snp.bottom).inset(20)
+        }
+        authorLabel.snp.makeConstraints {
+            $0.centerY.equalTo(dateLabel)
+            $0.left.equalTo(dateLabel.snp.right).offset(20)
+        }
+        
+    }
+
+    
+    override func setHighlighted(_ highlighted: Bool, animated: Bool) {
+        super.setHighlighted(highlighted, animated: animated)
+        
+        if isHighlighted == true {
+            contentView.backgroundColor = UIColor(red: 227 / 255.0, green: 241 / 255.0, blue: 223 / 255.0 , alpha: 1.0)
+        } else {
+            contentView.backgroundColor = .white
+        }
+    }
+    
+}
---
+++ NUTEE/Screens/Notice/NoticeVC.swift
@@ -0,0 +1,127 @@
+//
+//  NoticeVC.swift
+//  NUTEE
+//
+//  Created by Junhyeon on 2020/07/21.
+//  Copyright © 2020 Nutee. All rights reserved.
+//
+
+import UIKit
+
+import SwiftKeychainWrapper
+
+import SnapKit
+import Then
+
+class NoticeVC: UIViewController {
+    
+    // MARK: - UI components
+    
+    let menuBar = MenuBarCV()
+    
+    let noticeFeedContainerCollectionView = UICollectionView(frame: .zero, collectionViewLayout: UICollectionViewFlowLayout())
+    
+    // MARK: - Variables and Properties
+    
+    // MARK: - Life Cycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.navigationItem.title = "공지사항"
+        view.backgroundColor = .white
+        
+        setMenuBar()
+        setNoticeFeedContainerCollectionView()
+    }
+    
+    // MARK: - Helper
+    
+    func setMenuBar() {
+        _ = menuBar.then {
+            view.addSubview($0)
+            
+            $0.snp.makeConstraints {
+                $0.top.equalTo(view.snp.top)
+                $0.left.equalTo(view.snp.left)
+                $0.right.equalTo(view.snp.right)
+                $0.height.equalTo(50)
+            }
+            
+            $0.menuList = ["학사", "수업", "학점", "장학", "일반", "행사"]
+            $0.noticeVC = self
+        }
+    }
+    
+    func setNoticeFeedContainerCollectionView() {
+        _ = noticeFeedContainerCollectionView.then {
+            let layout = UICollectionViewFlowLayout()
+            layout.scrollDirection = .horizontal
+            layout.minimumLineSpacing = 0
+            layout.minimumInteritemSpacing = 0
+            
+            $0.collectionViewLayout = layout
+            
+            $0.delegate = self
+            $0.dataSource = self
+            
+            $0.register(NoticeFeedContainerCVCell.self, forCellWithReuseIdentifier: Identify.NoticeFeedContainerCVCell)
+            
+            view.addSubview($0)
+            $0.snp.makeConstraints {
+                $0.top.equalTo(menuBar.snp.bottom)
+                $0.left.equalTo(view.snp.left)
+                $0.right.equalTo(view.snp.right)
+                $0.bottom.equalTo(view.snp.bottom)
+            }
+            
+            $0.backgroundColor = .white
+            
+            $0.isPagingEnabled = true
+            $0.showsHorizontalScrollIndicator = false
+        }
+    }
+    
+    func scrollToMenuIndex(menuIndex: Int) {
+        let indexPath = IndexPath(item: menuIndex, section: 0)
+        noticeFeedContainerCollectionView.scrollToItem(at: indexPath, at: .centeredHorizontally, animated: true)
+    }
+
+}
+
+// MARK: - NoticeFeed CollectionView Container
+
+extension NoticeVC : UICollectionViewDelegate { }
+extension NoticeVC : UICollectionViewDelegateFlowLayout {
+    
+    func collectionView(_ collectionView:UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: self.view.frame.width, height: collectionView.frame.height)
+    }
+
+}
+
+extension NoticeVC : UICollectionViewDataSource {
+    
+    func scrollViewDidScroll(_ scrollView: UIScrollView) {
+        menuBar.positionBarView.frame.origin.x = scrollView.contentOffset.x / CGFloat(menuBar.menuList.count) + 10
+    }
+    
+    func scrollViewWillEndDragging(_ scrollView: UIScrollView, withVelocity velocity: CGPoint, targetContentOffset: UnsafeMutablePointer<CGPoint>) {
+        let menuIndex = Int(targetContentOffset.pointee.x / view.frame.width)
+        let indexPath = IndexPath(item: menuIndex, section: 0)
+        menuBar.menuBarCollectionView.selectItem(at: indexPath, animated: true, scrollPosition: .centeredHorizontally)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return menuBar.menuList.count
+    }
+
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: Identify.NoticeFeedContainerCVCell, for: indexPath) as! NoticeFeedContainerCVCell
+
+        cell.noticeVC = self
+        cell.setTableView()
+
+        return cell
+    }
+}
