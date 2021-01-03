58cdefa29a1a3927fe418bd54ebbd1e2fb005cc7
Kim HeeJae <iowa329@gmail.com>
2021-01-03 20:49:59 +0900

    APIConstants 정리

--- NUTEE/Global/Services/APIConstants.swift
+++ NUTEE/Global/Services/APIConstants.swift
@@ -8,20 +8,20 @@
 
 struct APIConstants {
     
+    // BaseURL
     static let BaseURL = "http://13.124.232.115:9596"
     static let TestURL = "http://13.124.232.115:9425"
+    
     static let AuthURL = "http://3.34.61.71:9708"
     
-    // Notice
-    static let NoticeBaseURL = "http://nutee.kr:9709/crawl"
+    // Login
+    static let Login = AuthURL + "/auth/login"
+    static let Logout = AuthURL + "/auth/logout"
     
-    static let NoticeBachelor = NoticeBaseURL + "/haksa"
-    static let NoticeClass = NoticeBaseURL + "/sooup"
-    static let NoticeExchange = NoticeBaseURL + "/hakjum"
-    static let NoticeScholarship = NoticeBaseURL + "/janghak"
-    static let NoticeGeneral = NoticeBaseURL + "/ilban"
-    static let NoticeEvent = NoticeBaseURL + "/hangsa"
-
+    static let FindID = AuthURL + "/auth/findid"
+    static let FindPW = AuthURL + "/auth/findpw"
+    
+    // SignUp
     static let SignUp = AuthURL + "/auth/signup"
     
     static let OTPSend = AuthURL + "/auth/sendotp"
@@ -29,11 +29,13 @@ struct APIConstants {
     static let IDCheck = AuthURL + "/auth/checkid"
     static let NickCheck = AuthURL + "/auth/checknickname"
 
-    static let Login = AuthURL + "/auth/login"
-    // POST { userId : id , password: pw }
-    static let Logout = AuthURL + "/auth/logout"
-    // POST
-    static let FindID = AuthURL + "/auth/findid"
-    static let FindPW = AuthURL + "/auth/findpw"
+    // Notice
+    static let NoticeURL = "http://nutee.kr:9709/crawl"
     
+    static let NoticeBachelor = NoticeURL + "/haksa"
+    static let NoticeClass = NoticeURL + "/sooup"
+    static let NoticeExchange = NoticeURL + "/hakjum"
+    static let NoticeScholarship = NoticeURL + "/janghak"
+    static let NoticeGeneral = NoticeURL + "/ilban"
+    static let NoticeEvent = NoticeURL + "/hangsa"
 }
