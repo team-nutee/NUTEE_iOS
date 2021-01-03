3c6839dcca5832d9cdb549052f6eb042876934fb
Kim HeeJae <iowa329@gmail.com>
2021-01-03 14:34:30 +0900

    [CREATE] 공통 SignUp ViewController 파일 구성

---
+++ NUTEE/Screens/Login/SingUp/SignUpViewController.swift
@@ -0,0 +1,273 @@
+//
+//  UISignUpViewController.swift
+//  NUTEE
+//
+//  Created by Hee Jae Kim on 2021/01/02.
+//  Copyright © 2021 Nutee. All rights reserved.
+//
+
+import UIKit
+import SnapKit
+
+// Define of common SigUp Style ViewController
+class SignUpViewController: UIViewController {
+    
+    // MARK: - UI components
+    
+    // common UI components when SignUp
+    let closeButton = HighlightedButton()
+    
+    let progressView = UIProgressView()
+    
+    let guideLabel = UILabel()
+    
+    let nextButton = HighlightedButton()
+    let previousButton = HighlightedButton()
+    
+    // MARK: - Variables and Properties
+    
+    // common variables when SignUp(progressBar, animation, constraints-positions)
+    var totalSignUpViews: Float = 0.0
+    var progressStatusCount: Float = 0.0
+    
+    var animationDuration: TimeInterval = 1.4
+    let xPosAnimationRange: CGFloat = 50
+    let yPosAnimationRange: CGFloat = 50
+    
+    var previousButtonBottomConstraint: Constraint?
+    
+    // MARK: - Life Cycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        initCommonViews()
+        makeCommonConstraints()
+        
+        modalPresentationStyle = .fullScreen
+        modalTransitionStyle = .crossDissolve
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        
+        enterCommonViewsAnimate()
+    }
+    
+    // MARK: - Override Function
+    
+    override func present(_ viewControllerToPresent: UIViewController, animated flag: Bool, completion: (() -> Void)? = nil) {
+        switch viewControllerToPresent {
+        case is NuteeAlertDialogue, is NuteeAlertSheet:
+            viewControllerToPresent.modalPresentationStyle = .overCurrentContext
+        default:
+            viewControllerToPresent.modalPresentationStyle = .fullScreen
+        }
+        
+        super.present(viewControllerToPresent, animated: flag, completion: completion)
+    }
+    
+    // MARK: - Helper
+    
+    func initCommonViews() {
+        _ = view.then {
+            $0.backgroundColor = .white
+            $0.tintColor = .nuteeGreen
+        }
+        
+        _ = closeButton.then {
+            $0.setTitle("닫기", for: .normal)
+            $0.titleLabel?.font = .boldSystemFont(ofSize: 15)
+            $0.setTitleColor(.nuteeGreen, for: .normal)
+            
+            $0.addTarget(self, action: #selector(didTapCloseButton), for: .touchUpInside)
+        }
+        
+        _ = progressView.then {
+            $0.progressViewStyle = .bar
+            $0.tintColor = .nuteeGreen
+            $0.progress = progressStatusCount / totalSignUpViews
+            progressStatusCount += 1
+        }
+        
+        _ = guideLabel.then {
+            $0.text = "guideLabelContentArea"
+            $0.font = .boldSystemFont(ofSize: 20)
+            
+            $0.alpha = 0
+        }
+        
+        _ = previousButton.then {
+            $0.setTitle("이전", for: .normal)
+            $0.titleLabel?.font = .boldSystemFont(ofSize: 20)
+            $0.setTitleColor(.nuteeGreen, for: .normal)
+            
+            $0.addTarget(self, action: #selector(didTapPreviousButton), for: .touchUpInside)
+        }
+        _ = nextButton.then {
+            $0.setTitle("다음", for: .normal)
+            $0.titleLabel?.font = .boldSystemFont(ofSize: 20)
+            $0.setTitleColor(.nuteeGreen, for: .normal)
+            
+            $0.isEnabled = true
+            $0.setTitleColor(.veryLightPink, for: .normal)
+            
+            $0.addTarget(self, action: #selector(didTapNextButton), for: .touchUpInside)
+        }
+    }
+    
+    func makeCommonConstraints() {
+        // Add SubView
+        view.addSubview(closeButton)
+        
+        view.addSubview(progressView)
+        
+        view.addSubview(guideLabel)
+        
+        // specific views are here
+        
+        view.addSubview(previousButton)
+        view.addSubview(nextButton)
+        
+        
+        // Make Constraints
+        closeButton.snp.makeConstraints {
+            $0.width.equalTo(50)
+            $0.height.equalTo(closeButton.snp.width)
+            
+            $0.top.equalTo(view.safeAreaLayoutGuide.snp.top)
+            $0.left.equalTo(view.snp.left).offset(20)
+        }
+        
+        progressView.snp.makeConstraints {
+            $0.top.equalTo(closeButton.snp.bottom).offset(20)
+            $0.left.equalTo(view.snp.left)
+            $0.right.equalTo(view.snp.right)
+        }
+
+        guideLabel.snp.makeConstraints {
+            $0.top.equalTo(progressView.snp.bottom).offset(35 - yPosAnimationRange)
+            $0.left.equalTo(closeButton.snp.left)
+            $0.right.equalTo(view.snp.right).inset(20)
+        }
+        
+        // specific constraints are here
+        
+        previousButton.snp.makeConstraints {
+            $0.width.equalTo(view.frame.size.width / 2.0)
+            $0.height.equalTo(50)
+            
+            $0.left.equalTo(view.snp.left)
+            previousButtonBottomConstraint = $0.bottom.equalTo(view.safeAreaLayoutGuide.snp.bottom).constraint
+        }
+        nextButton.snp.makeConstraints {
+            $0.width.equalTo(previousButton.snp.width)
+            $0.height.equalTo(previousButton.snp.height)
+            
+            $0.centerY.equalTo(previousButton)
+            $0.left.equalTo(previousButton.snp.right)
+        }
+    }
+    
+    @objc func didTapCloseButton() {
+        let nuteeAlertDialogue = NuteeAlertDialogue()
+        nuteeAlertDialogue.windowWidth = 270
+        nuteeAlertDialogue.dialogueData = ["나가기", "입력하신 회원정보가 저장되지 않습니다\n그래도 나가시겠습니까?"]
+        nuteeAlertDialogue.okButtonData = ["나기기", UIColor.white, UIColor.red]
+        nuteeAlertDialogue.cancelButtonData[0] = "취소"
+        
+        nuteeAlertDialogue.addCancelSigUpAction()
+
+        nuteeAlertDialogue.modalPresentationStyle = .overCurrentContext
+        nuteeAlertDialogue.modalTransitionStyle = .crossDissolve
+        
+        present(nuteeAlertDialogue, animated: true)
+    }
+    
+    @objc func didTapPreviousButton() {
+        dismiss(animated: true)
+    }
+    
+    @objc func didTapNextButton() {
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        view.endEditing(true)
+    }
+    
+}
+
+// MARK: - PasswordVC Animation
+
+extension SignUpViewController {
+    
+    private func enterCommonViewsAnimate() {
+        // progressView
+        UIView.animate(withDuration: animationDuration,
+                       delay: 0,
+                       usingSpringWithDamping: 0.85,
+                       initialSpringVelocity: 1,
+                       options: [.curveEaseIn],
+                       animations: { [self] in
+                        progressView.setProgress(progressStatusCount / totalSignUpViews, animated: true)
+        })
+        
+        // guide title
+        UIView.animate(withDuration: animationDuration,
+                       delay: 1,
+                       usingSpringWithDamping: 0.6,
+                       initialSpringVelocity: 1,
+                       options: [.curveEaseIn],
+                       animations: {
+                        self.guideLabel.alpha = 1
+                        self.guideLabel.transform = CGAffineTransform.init(translationX: 0, y: 50)
+        })
+    }
+    
+}
+// MARK: - Keyboard
+
+extension SignUpViewController {
+    
+    func addKeyboardNotification() {
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide(_:)), name: UIResponder.keyboardWillHideNotification, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow(_:)), name: UIResponder.keyboardWillShowNotification, object: nil)
+    }
+    
+    @objc private func keyboardWillShow(_ notification: Notification)  {
+        if let info = notification.userInfo {
+            let duration = info[UIResponder.keyboardAnimationDurationUserInfoKey] as! TimeInterval
+            let curve = info[UIResponder.keyboardAnimationCurveUserInfoKey] as! UInt
+            let keyboardFrame = (info[UIResponder.keyboardFrameEndUserInfoKey] as! NSValue).cgRectValue
+            let keyboardHeight = keyboardFrame.height
+            let keyWindow = UIApplication.shared.connectedScenes
+            .filter({$0.activationState == .foregroundActive})
+            .map({$0 as? UIWindowScene})
+            .compactMap({$0})
+            .first?.windows
+            .filter({$0.isKeyWindow}).first
+            let bottomPadding = keyWindow?.safeAreaInsets.bottom
+            
+            previousButtonBottomConstraint?.layoutConstraints[0].constant = -(keyboardHeight - (bottomPadding ?? 0))
+            
+            self.view.setNeedsLayout()
+            UIView.animate(withDuration: duration, delay: 0, options: .init(rawValue: curve), animations: {
+                self.view.layoutIfNeeded()
+            })
+        }
+    }
+    
+    @objc private func keyboardWillHide(_ notification: Notification) {
+        if let info = notification.userInfo {
+            let duration = info[UIResponder.keyboardAnimationDurationUserInfoKey] as! TimeInterval
+            let curve = info[UIResponder.keyboardAnimationCurveUserInfoKey] as! UInt
+            
+            previousButtonBottomConstraint?.layoutConstraints[0].constant = 0
+            self.view.setNeedsLayout()
+            UIView.animate(withDuration: duration, delay: 0, options: .init(rawValue: curve), animations: {
+                self.view.layoutIfNeeded()
+            })
+        }
+    }
+    
+}
