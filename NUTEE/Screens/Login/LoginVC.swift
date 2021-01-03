941e2f40ca1b366ae0a9a1830749324f9f670c0f
Kim HeeJae <iowa329@gmail.com>
2021-01-03 14:46:31 +0900

    로그인 서버 연결 코드 재활성화

--- NUTEE/Screens/Login/LoginVC.swift
+++ NUTEE/Screens/Login/LoginVC.swift
@@ -51,7 +51,7 @@ class LoginVC: UIViewController {
         makeConstraints()
         
         enterLoginVCAnimate()
-//        checkSignIn()
+        checkSignIn()
     }
     
     
@@ -367,18 +367,6 @@ extension LoginVC {
                         self.idErrorLabel.transform = CGAffineTransform.init(translationX: 0, y: 0)
                         self.pwErrorLabel.transform = CGAffineTransform.init(translationX: 0, y: 0)
         })
-    }
-    
-}
-
-// MARK: - Custom Button
-// highlight 상황 시 tinColor나 alpha 값을 일반적인 방법으로는 변경 할 수 없어서 커스텀으로 버튼 설정
-class HighlightedButton: UIButton {
-
-    override var isHighlighted: Bool {
-        didSet {
-            alpha = isHighlighted ? 0.5 : 1.0
-        }
     }
     
 }
