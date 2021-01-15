e68547e611a3434cdf0bc839bb57af7a5ef243d4
Kim HeeJae <iowa329@gmail.com>
2021-01-15 23:32:15 +0900

    [UPDATE] console 오류 발생 문제 수정 및 댓글 창 레이아웃 기능 구현

--- NUTEE/Screens/Home/DetailNewsFeed/DetailNewsFeedVC.swift
+++ NUTEE/Screens/Home/DetailNewsFeed/DetailNewsFeedVC.swift
@@ -20,15 +20,13 @@ class DetailNewsFeedVC: UIViewController {
     let refreshControl = UIRefreshControl()
     
     let commentView = UIView()
-    
-    let commentTextView = UITextView()
-    let placeholderLabel = UILabel()
-    
+    let commentTextView = PlaceholderTextView()
     let submitButton = UIButton()
     
     //MARK: - Variables and Properties
-    // FeedTVC와 DetailHeadderView가 통신하기 위해 중간(DetailNewsFeed) 연결 델리게이트 변수 선언
-    var delegate: DetailHeaderViewDelegate?
+    
+    let commentTextViewFontSize: CGFloat = 14
+    let commentTextViewHeight: CGFloat = 100
     
     var post: PostContent?
     var postBody: PostContentBody?
@@ -48,7 +46,7 @@ class DetailNewsFeedVC: UIViewController {
         initView()
         makeConstraints()
         
-        setRefresh()
+//        setRefresh()
         
         addKeyboardNotification()
     }
@@ -60,7 +58,7 @@ class DetailNewsFeedVC: UIViewController {
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(false)
     }
-
+    
     // MARK: - Helper
     
     func initView() {
@@ -74,58 +72,54 @@ class DetailNewsFeedVC: UIViewController {
             $0.dataSource = self
             
             $0.register(DetailNewsFeedHeaderView.self, forHeaderFooterViewReuseIdentifier: Identify.DetailNewsFeedHeaderView)
-            $0.register(ReplyCell.self, forCellReuseIdentifier: Identify.ReplyCell)
+            $0.register(ReplyTVCell.self, forCellReuseIdentifier: Identify.ReplyTVCell)
             $0.register(NoReplyFooterView.self, forHeaderFooterViewReuseIdentifier: Identify.NoReplyFooterView)
             
             $0.backgroundColor = .white
             $0.separatorInset.left = 0
             $0.separatorStyle = .singleLine
             
-            $0.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(didTapScreen(sender:))))
+            $0.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(didTapOutsideOfCommentView(sender:))))
         }
         
         _ = commentView.then {
             $0.backgroundColor = .white
             $0.addBorder(.top, color: .veryLightPink, thickness: 0.3)
         }
-        
         _ = commentTextView.then {
             $0.delegate = self
             
-            $0.font = .systemFont(ofSize: 13)
-            $0.tintColor = .veryLightPink
-            $0.translatesAutoresizingMaskIntoConstraints = false
-        }
-        
-        _ = placeholderLabel.then {
-            $0.text = "댓글을 입력하세요"
-            $0.sizeToFit()
-            $0.font = .systemFont(ofSize: 14)
-            $0.textColor = .gray
-        }
-        
-        _ = submitButton.then {
-            //$0.setImage(UIImage(systemName: "arrow.up.circle.fill"), for: .normal)
-            $0.tintColor = .nuteeGreen
-            $0.backgroundColor = .gray
-            $0.setTitle("등록", for: .normal)
+            $0.placeholderLabel.text = "댓글을 입력하세요"
+            $0.placeholderLabel.font = .systemFont(ofSize: commentTextViewFontSize)
             
-            $0.alpha = 1
+            $0.font = .systemFont(ofSize: commentTextViewFontSize)
+            $0.tintColor = .nuteeGreen
+            
+            $0.translatesAutoresizingMaskIntoConstraints = false
+            $0.isScrollEnabled = false
+        }
+        _ = submitButton.then {
+            $0.setImage(UIImage(systemName: "arrow.up.circle.fill"), for: .normal)
+            $0.tintColor = .nuteeGreen
+            
+            $0.imageView?.contentMode = .scaleAspectFit
+            $0.contentHorizontalAlignment = .fill
+            $0.contentVerticalAlignment = .fill
+            $0.imageEdgeInsets = UIEdgeInsets(top: 0, left: 0, bottom: 10, right: 5)
+            
+            $0.alpha = 0
             
             $0.addTarget(self, action: #selector(didTapSubmitButton), for: .touchUpInside)
         }
     }
     
     func makeConstraints() {
-        
         view.addSubview(detailNewsFeedTableView)
 
         view.addSubview(commentView)
-        
         commentView.addSubview(commentTextView)
-        commentTextView.addSubview(placeholderLabel)
+        commentView.addSubview(submitButton)
         
-        //commentView.addSubview(submitButton)
         
         detailNewsFeedTableView.snp.makeConstraints {
             $0.top.equalTo(view.snp.top)
@@ -133,42 +127,28 @@ class DetailNewsFeedVC: UIViewController {
             $0.right.equalTo(view.snp.right)
         }
         
-        commentView.backgroundColor = .yellow
         commentView.snp.makeConstraints {
-            //$0.height.greaterThanOrEqualTo(50)
-            //$0.height.lessThanOrEqualTo(130)
-            //$0.height.lessThanOrEqualTo(100)
-            
             $0.top.equalTo(detailNewsFeedTableView.snp.bottom)
             $0.left.equalTo(view.snp.left)
             $0.right.equalTo(view.snp.right)
-            commentViewBottomConstraint = $0.bottom.equalTo(view.safeAreaLayoutGuide.snp.bottom).constraint
+            commentViewBottomConstraint =
+                $0.bottom.equalTo(view.safeAreaLayoutGuide.snp.bottom).constraint
         }
-        
         commentTextView.snp.makeConstraints {
-            //$0.height.greaterThanOrEqualTo(40)
-            $0.height.lessThanOrEqualTo(90)
+            $0.height.lessThanOrEqualTo(commentTextViewHeight)
             
             $0.top.equalTo(commentView.snp.top).offset(10)
             $0.left.equalTo(commentView.snp.left).offset(10)
-            $0.right.equalTo(commentView.snp.right).inset(10)
             $0.bottom.equalTo(commentView.snp.bottom).inset(10)
         }
+        submitButton.snp.makeConstraints {
+            $0.width.equalTo(40)
+            $0.height.equalTo(submitButton.snp.width)
 
-        placeholderLabel.snp.makeConstraints {
-            $0.top.equalTo(commentTextView.snp.top).offset(10)
-            
-            $0.centerY.equalTo(commentTextView)
+            $0.left.equalTo(commentTextView.snp.right)
+            $0.right.equalTo(commentView.snp.right)
+            $0.bottom.equalTo(commentView.snp.bottom)
         }
-
-//        submitButton.snp.makeConstraints {
-//            $0.width.equalTo(40)
-//            $0.height.equalTo(submitButton.snp.width)
-//
-//            $0.left.equalTo(commentTextView.snp.right).offset(5)
-//            $0.right.equalTo(commentView.snp.right)
-//            $0.bottom.equalTo(commentTextView.snp.bottom)
-//        }
     }
     
     func setRefresh() {
@@ -205,7 +185,7 @@ class DetailNewsFeedVC: UIViewController {
         }
     }
 
-    @objc func didTapScreen(sender: UITapGestureRecognizer) {
+    @objc func didTapOutsideOfCommentView(sender: UITapGestureRecognizer) {
         if sender.state == .ended {
             view.endEditing(true)
         }
@@ -214,22 +194,28 @@ class DetailNewsFeedVC: UIViewController {
 }
 
 //MARK: - Build TableView
+
 extension DetailNewsFeedVC : UITableViewDelegate, UITableViewDataSource {
 
     // HeaderView
+    func tableView(_ tableView: UITableView, estimatedHeightForHeaderInSection section: Int) -> CGFloat {
+        // 임의 값이 있어야 작동함
+        return 100
+    }
+
+    func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        return UITableView.automaticDimension
+    }
+    
     func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
-        // Dequeue with the reuse identifier
-        let detailNewsFeedHeaderView = tableView.dequeueReusableHeaderFooterView(withIdentifier: Identify.DetailNewsFeedHeaderView) as? DetailNewsFeedHeaderView
-        detailNewsFeedHeaderView?.detailNewsFeedVC = self
-
-        detailNewsFeedHeaderView?.initHeaderView()
-        detailNewsFeedHeaderView?.addContentView()
-
+        let headerView = tableView.dequeueReusableHeaderFooterView(withIdentifier: Identify.DetailNewsFeedHeaderView) as? DetailNewsFeedHeaderView
+        headerView?.detailNewsFeedVC = self
+        
         // HeaderView로 NewsFeedVC에서 받아온 게시글 정보룰 넘김
-        detailNewsFeedHeaderView?.post = self.post
+        headerView?.post = self.post
         //detailNewsFeedHeaderView?.initPosting()
 
-        return detailNewsFeedHeaderView
+        return headerView
     }
 
     // Cell
@@ -242,104 +228,88 @@ extension DetailNewsFeedVC : UITableViewDelegate, UITableViewDataSource {
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return post?.body.comments?.count ?? 0
+        return 5//post?.body.comments?.count ?? 0
     }
 
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: Identify.ReplyCell, for: indexPath) as! ReplyCell
+        let cell = tableView.dequeueReusableCell(withIdentifier: Identify.ReplyTVCell, for: indexPath) as! ReplyTVCell
         cell.selectionStyle = .none
+        
         cell.detailNewsFeedVC = self
-        cell.delegate = self
-        
         cell.comment = post?.body.comments?[indexPath.row]
-        
-        cell.initCell()
-        cell.addContentView()
-        cell.fillDataToView()
 
         return cell
     }
 
     // FooterView
+    func tableView(_ tableView: UITableView, estimatedHeightForFooterInSection section: Int) -> CGFloat {
+        // 임의 값이 있어야 작동함
+        return 2
+    }
+    
     func tableView(_ tableView: UITableView, heightForFooterInSection section: Int) -> CGFloat {
-        if post?.body.comments?.count == 0 {
-            return 200.0
-        } else {
-            return .leastNormalMagnitude
-        }
+        
+//        if post?.body.comments?.count == 0 {
+        return UITableView.automaticDimension
+//        } else {
+//            return 0
+//        }
     }
 
-    // 댓글이 없을 때 띄우는 footer view
+    // 댓글이 없을 때 표시 할 정보
     func tableView(_ tableView: UITableView, viewForFooterInSection section: Int) -> UIView? {
-        let noReplyFooterView = tableView.dequeueReusableHeaderFooterView(withIdentifier: Identify.NoReplyFooterView) as? NoReplyFooterView
+        let footerView = tableView.dequeueReusableHeaderFooterView(withIdentifier: Identify.NoReplyFooterView) as? NoReplyFooterView
         
-        noReplyFooterView?.initFooterView()
-        noReplyFooterView?.addContentView()
-
-        return noReplyFooterView
+        return footerView
     }
 }
 
 // MARK: - UITextView Delegate
+
 extension DetailNewsFeedVC: UITextViewDelegate {
     
-    // TextView의 동적인 크기 변화를 위한 function
-    func textViewDidChange(_ textView: UITextView) {
-        let size = CGSize(width: view.frame.width, height: .infinity)
-        let estimatedSize = textView.sizeThatFits(size)
-        textView.constraints.forEach { (constraint) in
-            if constraint.firstAttribute == .height {
-                constraint.constant = estimatedSize.height
-            }
-        }
+    public func textViewDidChange(_ textView: UITextView) {
         
-        // 빈칸이나 줄바꿈으로만 입력된 경우 버튼 비활성화
+        commentTextView.handlePlaceholder()
+        
+        
+        // 전송 버튼 활성화(빈칸이나 줄바꿈으로만 입력된 경우 비활성화) 조건
         var str = commentTextView.text.replacingOccurrences(of: " ", with: "")
         str = str.replacingOccurrences(of: "\n", with: "")
-    
-        if str.count != 0 || str != "" {
-            self.placeholderLabel.isHidden = true
-            
-            UIView.animate(withDuration: 0.2) {
-                self.submitButton.alpha = 1.0
-                        }
-            
-        } else if str == "" {
+        if str.isEmpty == true {
             UIView.animate(withDuration: 0.1) {
                 self.submitButton.alpha = 0.0
-                        }
-        }
-        
-        let lineCounter = commentTextView.text.components(separatedBy: "\n").count - 1
-        if lineCounter <= 4 {
-            self.commentTextView.translatesAutoresizingMaskIntoConstraints = false
+            }
         } else {
-            self.commentTextView.translatesAutoresizingMaskIntoConstraints = true
+            UIView.animate(withDuration: 0.2) {
+                self.submitButton.alpha = 1.0
+            }
         }
+
         
-        // 댓글 입력창의 높이가 100 이상일 시 스크롤 기능 활성화
-        if commentTextView.contentSize.height >= 90 {
-            commentTextView.isScrollEnabled = true
+        // 댓글 입력창의 높이가 100 이상 넘을 시 스크롤 가능 활성화
+        if commentTextView.contentSize.height > commentTextViewHeight - commentTextViewFontSize {
+            if commentTextView.text.isEmpty == false {
+                commentTextView.translatesAutoresizingMaskIntoConstraints = true
+                commentTextView.isScrollEnabled = true
+            }
         } else {
-            commentTextView.frame.size.height = commentTextView.contentSize.height
+            commentTextView.translatesAutoresizingMaskIntoConstraints = false
             commentTextView.isScrollEnabled = false
+            
+            commentTextView.frame.size.height = commentTextView.contentSize.height
         }
-    }
-    
-    func textViewDidBeginEditing(_ textView: UITextView){
-        placeholderLabel.isHidden = true
         
-        textView.becomeFirstResponder()
-    }
-    
-    func textViewDidEndEditing(_ textView: UITextView) {
-        if commentTextView.text == "" {
-            placeholderLabel.isHidden = false
+        
+        UIView.animate(withDuration: 0.1) {
+            self.view.layoutIfNeeded()
         }
     }
+    
 }
 
 // MARK: - KeyBoard
+
 extension DetailNewsFeedVC {
 
     func addKeyboardNotification() {
@@ -387,16 +357,8 @@ extension DetailNewsFeedVC {
     }
 }
 
-// MARK: - ReplyCell과 통신하여 게시글 삭제 후 테이블뷰 정보 다시 로드하기
-extension DetailNewsFeedVC: ReplyCellDelegate {
-    func updateReplyTV() {
-        self.getPostService(postId: self.postId ?? 0, completionHandler: {(returnedData)-> Void in
-            self.detailNewsFeedTableView.reloadData()
-        })
-    }
-}
-
 // MARK: - Server connect
+
 extension DetailNewsFeedVC {
     
     // 게시글 한 개 가져오기
