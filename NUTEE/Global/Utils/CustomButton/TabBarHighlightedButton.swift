2074f61e3967bebe650a1f26c5cc5617323d3485
Kim HeeJae <iowa329@gmail.com>
2021-02-04 11:08:43 +0900

    TabBar에 적용 할 수 있는 highlightedButton 클래스 구성

--- NUTEE/Global/Utils/CustomButton/TabBarHighlightedButton.swift
+++ NUTEE/Global/Utils/CustomButton/TabBarHighlightedButton.swift
@@ -6,4 +6,34 @@
 //  Copyright © 2021 Nutee. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+class TabBarHighlightedButton: UIButton {
+
+    // MARK: - Life Cycle
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        tintColor = .gray
+        
+        imageEdgeInsets = UIEdgeInsets(top: 0, left: 0, bottom: 7, right: 0)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    // MARK: - Override Function
+    
+    override var isHighlighted: Bool {
+        didSet {
+            if isHighlighted == true {
+                    tintColor = .nuteeGreen
+                } else {
+                    tintColor = .gray
+                }
+        }
+    }
+    
+}
