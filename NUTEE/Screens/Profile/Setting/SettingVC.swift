c6b7f3d3aa92a8258da0a774cf6f83c4847730c5
Kim HeeJae <iowa329@gmail.com>
2021-02-24 01:07:42 +0900

    정리

--- NUTEE/Screens/Profile/Setting/SettingVC.swift
+++ NUTEE/Screens/Profile/Setting/SettingVC.swift
@@ -230,8 +230,6 @@ class SettingTVCell : UITableViewCell {
     
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
-        
-        
     }
     
     required init?(coder: NSCoder) {
