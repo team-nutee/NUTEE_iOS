f032cc3a296d7fbb0f1c5be6fda9acc898a34b83
Kim HeeJae <iowa329@gmail.com>
2021-03-08 23:35:46 +0900

    [CREATE] ReplyList 파일 구성

---
+++ NUTEE/Global/Enum/ReplyType+Enum.swift
@@ -0,0 +1,14 @@
+//
+//  KindOfComment+Enum.swift
+//  NUTEE
+//
+//  Created by Hee Jae Kim on 2021/03/08.
+//  Copyright © 2021 Nutee. All rights reserved.
+//
+
+import Foundation
+
+enum ReplyType {
+    case comment
+    case reComment
+}
